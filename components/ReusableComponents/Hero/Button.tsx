a3090ed886a1fb5d3e9e303775f5d861576bc6bd
BALASANTHOSH01 <santhos01ac@gmail.com>
2024-07-23 09:41:39 +0530

    Sync AI v1

--- components/ReusableComponents/Hero/Button.tsx
+++ components/ReusableComponents/Hero/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = ({text}) => {
+const Button = ({text}:any) => {
   return (
     <div>
         <div className='w-[150px] p-3 text-center rounded-[25px] border-[#11accb] border cursor-pointer hover:shadow-lg'>
