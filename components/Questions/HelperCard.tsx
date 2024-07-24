4bb482e1845f2f61c831265b4dd405b560c84a8a
BALASANTHOSH01 <santhos01ac@gmail.com>
2024-07-24 09:41:15 +0530

    parallex scrolling

--- components/Questions/HelperCard.tsx
+++ components/Questions/HelperCard.tsx
@@ -1,37 +1,51 @@
-import React from 'react'
-import { PlusIcon, SubtractionIcon } from '../ReusableComponents/IconList'
+import React from 'react';
+import { PlusIcon, SubtractionIcon } from '../ReusableComponents/IconList';
 
-const HelperCard = ({contentIs,handleGeneralFAQ,handleWebsiteFAQ}:any) => {
-  return (
-    <div className='w-[220px] h-[220px] text-white bg-[--consotiumTableBG] rounded-[20px] mt-[3%] relative'>
-      <div className=' flex flex-col gap-5 py-[8%] p-3 '>
-        <p className='ml-[2%]'>Whitepaper</p>
-
-        <hr className='h-[1px]' />
-
-        <div className='ml-[2%] flex flex-row items-center justify-between relative z-50'>
-          <p>General FAQ </p>
-          {
-            contentIs === "generalfaq" ? <SubtractionIcon className='cursor-pointer' onClick={()=>handleGeneralFAQ("websitefaq")}/> : <PlusIcon className='cursor-pointer' onClick={()=>handleWebsiteFAQ("generalfaq")}/>
-          }
-          
-        </div>
-
-        <hr className='h-[1px]' />
-
-        <div className='ml-[2%] flex flex-row items-center justify-between relative z-50'>
-          <p>Website & XP</p>
-          {
-            contentIs === "websitefaq" ? <SubtractionIcon className='cursor-pointer' onClick={()=>handleGeneralFAQ("generalfaq")}/> : <PlusIcon className='cursor-pointer' onClick={()=>handleWebsiteFAQ("websitefaq")}/>
-          }
-        </div>
-
-
-      </div>
-
-        <div className='bg-gradient-to-b from-transparent to-[--darkBg] w-[250px] h-[150px] blur-sm absolute bottom-[-5px] left-[-5px]'></div>
-    </div>
-  )
+interface HelperCardProps {
+  contentIs: string;
+  handleGeneralFAQ: (data: string) => void;
+  handleWebsiteFAQ: (data: string) => void;
 }
 
-export default HelperCard
+const HelperCard: React.FC<HelperCardProps> = ({ contentIs, handleGeneralFAQ, handleWebsiteFAQ }) => {
+  return (
+    <div className='w-[220px] h-[220px] text-white bg-[--consotiumTableBG] rounded-[20px] mt-[3%] relative'>
+      <div className='flex flex-col gap-5 py-[8%] p-3'>
+        <p className='ml-[2%]'>Whitepaper</p>
+        <hr className='h-[1px]' />
+        <div className='ml-[2%] flex flex-row items-center justify-between relative z-50'>
+          <p>General FAQ</p>
+          {contentIs === 'generalfaq' ? (
+            <SubtractionIcon
+              className='cursor-pointer text-[18px]'
+              onClick={() => handleWebsiteFAQ('websitefaq')}
+            />
+          ) : (
+            <PlusIcon
+              className='cursor-pointer'
+              onClick={() => handleGeneralFAQ('generalfaq')}
+            />
+          )}
+        </div>
+        <hr className='h-[1px]' />
+        <div className='ml-[2%] flex flex-row items-center justify-between relative z-50'>
+          <p>Website & XP</p>
+          {contentIs === 'websitefaq' ? (
+            <SubtractionIcon
+              className='cursor-pointer text-[18px]'
+              onClick={() => handleGeneralFAQ('generalfaq')}
+            />
+          ) : (
+            <PlusIcon
+              className='cursor-pointer'
+              onClick={() => handleWebsiteFAQ('websitefaq')}
+            />
+          )}
+        </div>
+      </div>
+      <div className='bg-gradient-to-b from-transparent to-[--darkBg] w-[250px] h-[150px] blur-sm absolute bottom-[-5px] left-[-5px]'></div>
+    </div>
+  );
+};
+
+export default HelperCard;
--- components/Questions/Questions.tsx
+++ components/Questions/Questions.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { useParallax, ParallaxProvider } from 'react-scroll-parallax';
-import HelperCard from "./HelperCard";
-import { GeneralFAQ, WebsiteFAQ } from "./QuestionsList";
+import HelperCard from './HelperCard';
+import { GeneralFAQ, WebsiteFAQ } from './QuestionsList';
 
-const Questions = () => {
-  const [contentIs, setContentIs] = useState<string>("generalfaq");
+const Questions: React.FC = () => {
+  const [contentIs, setContentIs] = useState<string>('generalfaq');
 
   const handleContentChange = (data: string) => {
     setContentIs(data);
@@ -14,9 +14,9 @@ const Questions = () => {
 
   const renderFAQContent = (faqList: { question: string; answer: string }[]) => {
     return faqList.map((faq, index) => (
-      <div className="flex flex-col gap-2 my-[2%]" key={index}>
-        <p className="font-bold text-[24px]">{faq.question}</p>
-        <p className="text-[14px]">{faq.answer}</p>
+      <div className='flex flex-col gap-2 my-[2%]' key={index}>
+        <p className='font-bold text-[24px]'>{faq.question}</p>
+        <p className='text-[14px]'>{faq.answer}</p>
       </div>
     ));
   };
@@ -26,20 +26,16 @@ const Questions = () => {
 
   return (
     <ParallaxProvider>
-      <div className="py-[4%] mb-[15%]">
-        <p ref={parallaxFAQHeader.ref} className="text-[40px] text-center">Frequently Asked Questions</p>
-
-        <div className="flex flex-row items-start justify-center gap-5 my-[5%]">
+      <div className='py-[4%] mb-[15%]'>
+        <p ref={parallaxFAQHeader.ref} className='text-[40px] text-center'>Frequently Asked Questions</p>
+        <div className='flex flex-row items-start justify-center gap-5 my-[5%]'>
           <HelperCard
             contentIs={contentIs}
-            handleGeneralFAQ={() => handleContentChange("generalfaq")}
-            handleWebsiteFAQ={() => handleContentChange("websitefaq")}
+            handleGeneralFAQ={handleContentChange}
+            handleWebsiteFAQ={handleContentChange}
           />
-
-          <div ref={parallaxFAQContent.ref} className="w-[60%] z-50 relative">
-            {contentIs === "generalfaq"
-              ? renderFAQContent(GeneralFAQ)
-              : renderFAQContent(WebsiteFAQ)}
+          <div ref={parallaxFAQContent.ref} className='w-[60%] z-50 relative'>
+            {contentIs === 'generalfaq' ? renderFAQContent(GeneralFAQ) : renderFAQContent(WebsiteFAQ)}
           </div>
         </div>
       </div>
